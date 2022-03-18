9df4a0194b96064d5829ff13df34c168306f614c
TsalachSnow <prosper.amalaha@decagon.dev>
2022-03-18 02:27:38 +0100

    implementing clickHandler and using useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expense from './components/Expenses/Expense';
 
 function App() {
 const expense = [
@@ -32,26 +31,7 @@ const expense = [
   return (
     <div>
       <h2>Let's get started!!</h2>
-      <ExpenseItem 
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem 
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem 
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem 
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <Expense item={expense}/>
     </div>
   );
 }
